b6171b8c180103094630696be6ce1d019ece0011
Yines <ginesgonzalezbrusca@gmail.com>
2026-05-10 12:16:11 +0200

    Add ESLint configuration file
    Configure ESLint with specific rules and global variables.

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+module.exports = [
+  {
+    languageOptions: {
+      globals: 
+        window: "readonly",
+        document: "readonly",
+        console: "readonly",
+        module: "readonly",
+        require: "readonly",
+        describe: "readonly",
+        it: "readonly",
+        chai: "readonly"
+      }
+    },
+    rules: {
+      "no-cond-assign": ["error", "always"],
+      "indent": ["error", 2],
+      "linebreak-style": ["error", "unix"],
+      "quotes": ["error", "single"],
+      "no-unused-vars": "off",
+      "semi": ["error", "always"]
+    }
+  }
+];
